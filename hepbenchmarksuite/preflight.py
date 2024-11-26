0372cace089cf2b5a116d240bf6d0356fda6ff20
Jessy Sobreiro <jessy.sobreiro@cern.ch>
2024-11-26 11:37:38 +0100

    [BMK-1549] Avoid failing when no hw/sw requirements set

--- hepbenchmarksuite/preflight.py
+++ hepbenchmarksuite/preflight.py
@@ -17,11 +17,23 @@ class Preflight:
 
     def __init__(self, config):
         self.benchmarks_to_run = config['global']['benchmarks']
-        self.global_config       = config['global']
-        self.hw_config           = config['global']['hw_requirements']
-        self.sw_config           = config['global']['sw_requirements']
-        self.full_config         = config
-        self.failed_checks              = []
+        self.global_config = config['global']
+        self.full_config = config
+        self.failed_checks = []
+
+        try:
+            self.hw_config = config['global']['hw_requirements']
+            _log.info("Hardware requirements configuration found")
+        except KeyError:
+            _log.info(
+                "No hardware requirements configuration specified, skipping")
+
+        try:
+            self.sw_config = config['global']['sw_requirements']
+            _log.info("Software requirements configuration found")
+        except KeyError:
+            _log.info(
+                "No software requirements configuration specified, skipping")
 
     def check(self):
         """Perform pre-flight checks."""
@@ -32,10 +44,14 @@ class Preflight:
         self.check_run_mode()
         self.check_working_directories()
         self.validate_spec_config()
-        self.check_disk_space()
-        self.check_mem_per_core()
-        self.check_selinux_disabled()
-        self.check_root_access()
+
+        if 'hw_requirements' in self.global_config:
+            self.check_disk_space()
+            self.check_mem_per_core()
+
+        if 'sw_requirements' in self.global_config:
+            self.check_selinux_disabled()
+            self.check_root_access()
 
         # Check if any pre-flight check failed
         if any(self.failed_checks):
@@ -53,7 +69,6 @@ class Preflight:
                 _log.warning("Your 'hs06' configuration is under the legacy key 'hepspec06'. "
                              "Please change it to 'hs06'.")
                 self.full_config['hs06'] = self.full_config['hepspec06']
-
 
     def validate_amq_config(self):
         """ Check if the private key used for AMQ is password protected and warn the user if so """
@@ -76,20 +91,22 @@ class Preflight:
             return
         _log.info(" - core count: %i", int(cpus))
 
-        disk_stats          = shutil.disk_usage(self.global_config['rundir'])
-        disk_space_gb       = round(disk_stats.free * (10 ** -9), 2)
+        disk_stats = shutil.disk_usage(self.global_config['rundir'])
+        disk_space_gb = round(disk_stats.free * (10 ** -9), 2)
         disk_space_per_core = disk_space_gb/int(cpus)
         _log.info("Calculated disk space: %s GB, GB per core: %s",
-                   *(disk_space_gb, disk_space_per_core))
+                  *(disk_space_gb, disk_space_per_core))
 
         if 'min_disk_per_core' not in self.hw_config:
-            #_log.warning("Hardware requirement configuration missing: 'min_disk_per_core'")
-            _log.error("Hardware requirement configuration missing: 'min_disk_per_core'")
+            # _log.warning("Hardware requirement configuration missing: 'min_disk_per_core'")
+            _log.error(
+                "Hardware requirement configuration missing: 'min_disk_per_core'")
             # either set a default so it runs or return an error
             # for now we'll fail the check
             self.failed_checks.append(1)
 
-        running_only_db12 = len(self.benchmarks_to_run) == 1 and 'db12' in self.benchmarks_to_run
+        running_only_db12 = len(
+            self.benchmarks_to_run) == 1 and 'db12' in self.benchmarks_to_run
         if disk_space_per_core <= self.hw_config.get('min_disk_per_core') and not running_only_db12:
             _log.error("Not enough disk space on %s, free: %s GB per core, required: %s GB/core",
                        self.global_config['rundir'], disk_space_per_core, self.hw_config.get('min_disk_per_core'))
@@ -203,7 +220,8 @@ class Preflight:
         _log.info(" - Checking for a valid configuration...")
         for bench in self.benchmarks_to_run:
             if bench in ('hs06', 'spec2017'):
-                self.failed_checks.append(benchmarks.validate_spec(self.full_config, bench))
+                self.failed_checks.append(
+                    benchmarks.validate_spec(self.full_config, bench))
 
     def check_working_directories(self):
         """ Ensure the specified working directories exist """
@@ -215,10 +233,12 @@ class Preflight:
             makedirs(self.full_config['hs06']['hepspec_volume'], exist_ok=True)
 
         if 'spec2017' in self.benchmarks_to_run:
-            makedirs(self.full_config['spec2017']['hepspec_volume'], exist_ok=True)
+            makedirs(self.full_config['spec2017']
+                     ['hepspec_volume'], exist_ok=True)
 
         if 'hepscore' in self.benchmarks_to_run:
-            makedirs(path.join(self.global_config['rundir'], "HEPSCORE"), exist_ok=True)
+            makedirs(
+                path.join(self.global_config['rundir'], "HEPSCORE"), exist_ok=True)
 
     def check_run_mode(self):
         """ Check whether the selected executor is installed in the system """
@@ -233,11 +253,13 @@ class Preflight:
             system_run_mode = shutil.which(mode)
 
             if system_run_mode is not None:
-                _log.info("   - %s executable found: %s.", mode, system_run_mode)
+                _log.info("   - %s executable found: %s.",
+                          mode, system_run_mode)
 
                 # TODO: change to match/case when moving to Python3.10
                 if mode == 'docker':
-                    version, _ = utils.exec_cmd("docker version --format '{{.Server.Version}}'")
+                    version, _ = utils.exec_cmd(
+                        "docker version --format '{{.Server.Version}}'")
                     self.check_docker_version(version)
 
                 elif mode == 'singularity':
@@ -257,14 +279,17 @@ class Preflight:
         cpus = self.global_config.get('ncores')
         if not cpus:
             # problem getting CPUs from config or it's unconfigured try again with nproc
-            _log.info(" - ncores not found in configuration, gathering core count from nproc")
+            _log.info(
+                " - ncores not found in configuration, gathering core count from nproc")
             cpus, _ = utils.exec_cmd('nproc')
         if not cpus:
             # if the config is empty and nproc fails we'll try one last time with os.cpu_count()
-            _log.info(" - nproc unable to determine core count, trying again with os.cpu_count")
+            _log.info(
+                " - nproc unable to determine core count, trying again with os.cpu_count")
             cpus = cpu_count()
         if not cpus:
             # unable to determine number of cpus from any source
-            _log.error(" - Unable to determine number of cores from system or configuration.")
+            _log.error(
+                " - Unable to determine number of cores from system or configuration.")
             return None
         return cpus
